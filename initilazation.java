e449e48972e9477c384f61704c7c893f8011825b
Pradeep Kumar <35121364+kumar2019@users.noreply.github.com>
2018-07-13 03:35:16 +0530

    Java file created

---
+++ initilazation.java
@@ -0,0 +1,25 @@
+package com.core;
+
+public class Initilization 
+{
+	private static String methodOne(String msg)
+	{
+		System.out.println(msg);
+		return msg;
+	}
+	static String m=methodOne("1");
+	{
+		m=methodOne("2");
+	}
+	static
+	{
+		m=methodOne("3");
+	}
+
+	public static void main(String[] args)
+	{
+     
+		Object obj=new Initilization();
+	}
+
+}
